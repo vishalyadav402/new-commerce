21d3e87d7951d152d730e5fc0b7157b476fa5882
vishalyadav402 <vishalyadav402@gmail.com>
2025-03-24 21:20:37 +0530

    Add files via upload

--- src/app/admin/component/page.js
+++ src/app/admin/component/page.js
@@ -1,24 +1,19 @@
-"use client";
-import { useEffect, useState } from "react";
-import { useRouter } from "next/navigation";
-import Loader from "../components/Loader";
+import AdminLayout from '@/app/AdminLayout'
+import React from 'react'
+import Whatsapp from '../component/Whatsapp'
 
 
-const Page = () => {
-  const router = useRouter();
-  const [loading, setLoading] = useState(true);
+const page = () => {
+  return (
+    <AdminLayout>
+        <div className='flex justify-center content-center h-full w-full'>
+            <div className='m-auto'>
+                <p className='text-center font-bold py-3 text-lg'>Scan with whatsapp web to login</p>
+                <Whatsapp/>
+            </div>
+        </div>
+    </AdminLayout>
+  )
+}
 
-  useEffect(() => {
-       router.replace("/account/profile");
-    }, [router]);
-
-  if (loading) {
-    return (
-      <Loader/>
-    );
-  }
-
-  return null;
-};
-
-export default Page;
+export default page
