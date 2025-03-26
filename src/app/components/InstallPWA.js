8329e115a1ef3afe1c1f2e70a097b36f99acb16a
Vishal yadav <vishsem@outlook.com>
2025-03-26 16:51:26 +0530

    chnages to installpwa.js

--- src/app/components/InstallPWA.js
+++ src/app/components/InstallPWA.js
@@ -1,4 +1,5 @@
 "use client"
+import { Box } from "@mui/material";
 import { useEffect, useState } from "react";
 
 const InstallPWA = () => {
@@ -34,12 +35,30 @@ const InstallPWA = () => {
 
   return (
     isVisible && (
-      <button
-        onClick={handleInstall}
-        className="fixed top-32 right-5 bg-purple-dark text-white px-4 py-2 rounded-md shadow-lg hover:bg-pink-dark"
-      >
-        Install App
-      </button>
+      <>
+        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[350px] bg-[#eee1f2] p-4 border border-purple-dark rounded-md text-center">
+        <p className="mb-3 text-2xl text-purple-dark font-semibold">
+          Install VegaCart Light
+        </p>
+        <p className="mb-2">
+          Get a better experience by installing our VegaCart Light app on your device.
+        </p>
+        <div className="flex gap-2 justify-center">
+          <button
+            className="bg-pink-dark px-4 py-1 rounded-md shadow-lg text-beige-light"
+            onClick={handleInstall}
+          >
+            Install
+          </button>
+          <button
+            className="border border-pink-dark px-4 py-1 rounded-md shadow-lg text-pink-dark"
+            onClick={setIsVisible(false)}
+          >
+            Skip
+          </button>
+        </div>
+      </div>
+      </>
     )
   );
 };
